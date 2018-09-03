accd14aa52a1357b26237e0311952ac73c508555
AlexxSoryn <32508402+AlexxSoryn@users.noreply.github.com>
2018-09-03 17:26:59 +0300

    Revert "Revert "Initial commit""
    This reverts commit 7de8c87a1ad43dd8f58f7ce339bd2e40f6563418.

---
+++ FileSecurity/Crypto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FileSecurity
+{
+    public class Crypto
+    {
+        Blowfish _blowfish;
+
+        public Crypto(byte[] key)
+        {
+            _blowfish = new Blowfish(BlowfishAlgorithm.CFB64);
+            _blowfish.SetKey(key);
+        }
+
+        public void Decrypt(byte[] packet)
+        {
+            byte[] buffer = _blowfish.Decrypt(packet);
+            System.Buffer.BlockCopy(buffer, 0, packet, 0, buffer.Length);
+        }
+
+        public void Encrypt(byte[] packet)
+        {
+            byte[] buffer = _blowfish.Encrypt(packet);
+            System.Buffer.BlockCopy(buffer, 0, packet, 0, buffer.Length);
+        }
+
+        public Blowfish Blowfish
+        {
+            get { return _blowfish; }
+        }
+    }
+}
