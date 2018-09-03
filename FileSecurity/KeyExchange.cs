5a2b87c13519f6f44d6991aedbe922076bd9ba44
AlexxSoryn <32508402+AlexxSoryn@users.noreply.github.com>
2018-09-03 17:33:57 +0300

    Revert "Revert "Revert "Revert "Initial commit""""
    This reverts commit 170345ae6b1bbf31ce3c56a68d15f196af075088.

---
+++ FileSecurity/KeyExchange.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FileSecurity
+{
+    public class KeyExchange
+    {
+      
+        public static byte[] ProcessPassword(byte[] password)
+        {
+            var hashService = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            var s1 = hashService.ComputeHash(password, 0, password.Length);
+            var s2 = hashService.ComputeHash(Encoding.ASCII.GetBytes(String.Concat(s1, s1)));
+            var sresult = String.Concat(s1, s2);
+
+            return ASCIIEncoding.Default.GetBytes(sresult);
+        }
+    }
+}
