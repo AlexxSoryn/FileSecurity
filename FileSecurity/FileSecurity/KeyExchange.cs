b3b212db479c1f17f0dac31f31e70da027421545
AlexxSoryn <32508402+AlexxSoryn@users.noreply.github.com>
2018-09-03 18:19:47 +0300

    FileSecurity

--- FileSecurity/FileSecurity/KeyExchange.cs
+++ FileSecurity/FileSecurity/KeyExchange.cs
@@ -13,7 +13,7 @@ namespace FileSecurity
             var hashService = new System.Security.Cryptography.MD5CryptoServiceProvider();
             var s1 = hashService.ComputeHash(password, 0, password.Length);
             var s2 = hashService.ComputeHash(Encoding.ASCII.GetBytes(String.Concat(s1, s1)));
-            var sresult = String.Concat(s1, s2);
+            var sresult = String.Concat(ASCIIEncoding.Default.GetString(s1), ASCIIEncoding.Default.GetString(s2));
 
             return ASCIIEncoding.Default.GetBytes(sresult);
         }
